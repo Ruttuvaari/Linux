6ac3eb1f224418a56211bdab0404c1c3719a6d4a
Ruttuvaari <114290372+Ruttuvaari@users.noreply.github.com>
2022-09-24 11:47:31 +0000

    Update user-overrides.js

--- Firefox/user-overrides.js
+++ Firefox/user-overrides.js
@@ -1,1 +1,14 @@
-//TEST
+// Ruttuvaari's overrides
+
+// Disable
+
+user_pref("extensions.pocket.enabled", false); //fully disable pocket
+user_pref("identity.fxaccounts.enabled", false); //disable sync
+user_pref("layout.spellcheckDefault", 0); //no spell-check
+user_pref("signon.rememberSignons", false); //never ask to save passwords
+
+// Enable
+
+user_pref("keyword.enabled", true); //search from the urlbar
+user_pref("browser.startup.page", 1); //Browser starts on the Homepage
+user_pref("browser.startup.homepage", "about:home"); //Sets homepage
