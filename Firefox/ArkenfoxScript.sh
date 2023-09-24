d45ac14bb307bf2c7f533365852fc5e8b8a021cf
Ruttuvaari <114290372+Ruttuvaari@users.noreply.github.com>
2023-09-24 14:05:24 +0000

    Update ArkenfoxScript.sh

--- Firefox/ArkenfoxScript.sh
+++ Firefox/ArkenfoxScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-############################################################################################################
-### This script creates a new Firefox profile and moves the necessary Arkenfox files to the profile folder. ###
-### The script also updates the user.js file with Ruttuvaari's user-overrides.                            ###
-### Version 0.5 - Ruttuvaari                                                                             ###
-############################################################################################################
+                ############################################################################################################
+                ### This script creates a new Firefox profile and moves the necessary Arkenfox files to the profile folder. ###
+                ### The script also updates the user.js file with Ruttuvaari's user-overrides.                            ###
+                ### Version 0.5 - Ruttuvaari                                                                             ###
+                ############################################################################################################
