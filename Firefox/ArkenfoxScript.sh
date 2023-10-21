444cfdbf2055f413726957c545620112f185c3e8
Ruttuvaari <114290372+Ruttuvaari@users.noreply.github.com>
2023-10-21 15:41:59 +0000

    Update ArkenfoxScript.sh

--- Firefox/ArkenfoxScript.sh
+++ Firefox/ArkenfoxScript.sh
@@ -11,7 +11,7 @@ NC='\033[0m' # No Color
 Temp="/home/$USER/FirefoxTemp"
 
 echo -e "${ORANGE}
-                ############################################################################################################
+                ###############################################################################################################
                 ### This script creates a new Firefox profile and moves the necessary Arkenfox files to the profile folder. ###
                 ### The script also updates the user.js file with Ruttuvaari's user-overrides.                            ###
                 ### Version 1.0 - Ruttuvaari                                                                             ###
