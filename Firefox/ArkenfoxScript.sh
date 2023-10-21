1d36ecd0f83d04299453f3eebee959279ba45237
Ruttuvaari <114290372+Ruttuvaari@users.noreply.github.com>
2023-10-21 15:43:19 +0000

    Update ArkenfoxScript.sh

--- Firefox/ArkenfoxScript.sh
+++ Firefox/ArkenfoxScript.sh
@@ -14,7 +14,7 @@ echo -e "${ORANGE}
                 ##################################################################################################
                 ### This script creates a new Firefox profile and moves the Arkenfox files to the profile folder. ###
                 ### The script also updates the user.js file with Ruttuvaari's user-overrides.                  ###
-                ### Version 1.0 - Ruttuvaari                                                                   ###
+                ### Version 1.0 - Ruttuvaari                                                                          ###
                 ##################################################################################################${NC}"
 
 echo -e "Status: ${GREEN}Shutting down firefox${NC}"
