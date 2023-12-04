eea5d19086efcbbb1f68daf9d07d9f19cdc4e01c
Ruttuvaari <114290372+Ruttuvaari@users.noreply.github.com>
2023-12-04 08:32:05 +0000

    Create update.sh

---
+++ Firefox/update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+### Variables ###
+
+### Colors ###
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+MAGENTA='\033[0;35m'
+NC='\033[0m' #No Color
+
+### Temp Folder ###
+Temp="/home/$USER/RuttuFoxTemp"
+
+### Files ###
+ArkenFox="https://github.com/arkenfox/user.js.git"
+RuttuFox="https://raw.githubusercontent.com/Ruttuvaari/Linux/main/Firefox/user-overrides.js"
+Dependencies="firefox git libva-mesa-driver lib32-libva-mesa-driver libva-utils ffmpeg nvtop"
+Directory="/home/$USER/.mozilla/firefox"
+
+# Shutting down Firefox
+echo -e "Status: ${GREEN}Shutting down Firefox${NC}"
+pkill -f firefox
